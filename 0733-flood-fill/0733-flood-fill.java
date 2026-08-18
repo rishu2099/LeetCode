b0e5d050b6e748f627938492fa80e48451d8a51b
Rishabh <221716443+rishu2099@users.noreply.github.com>
2026-08-18 09:57:12 +0530

    Time: 0 ms (100%), Space: 47 MB (11.29%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void helper(int[][] image, int sr, int sc, int color, boolean[][] vis, int orgCol) {
+        //base case 
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || vis[sr][sc] || image[sr][sc] != orgCol) {
+            return;
+        }
+        
+        vis[sr][sc] = true;
+        image[sr][sc] = color;
+
+        //left
+        helper(image, sr, sc-1, color, vis, orgCol);
+        //right
+        helper(image, sr, sc+1, color, vis, orgCol);
+        //up
+        helper(image, sr-1, sc, color, vis, orgCol);
+        //dowm
+        helper(image, sr+1, sc, color, vis, orgCol);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][] = new boolean[image.length][image[0].length];
+
+        helper(image, sr, sc, color, vis, image[sr][sc]);
+        return image;
+    }
+}
