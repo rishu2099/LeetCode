bb74d8175a2433d5bda3235c8476ddf28d74b0b3
Rishabh <221716443+rishu2099@users.noreply.github.com>
2026-09-02 22:07:34 +0530

    Time: 8 ms (46.92%), Space: 46.6 MB (95.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,28 +2,28 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
 
-        int[][] arr = new int[n][2];
-        for(int i = 0; i <  n; i++) {
+        int arr[][] = new int[n][2];
+        for(int i = 0; i < n; i++) {
             arr[i][0] = nums[i]; // value
-            arr[i][1] = i; // idx
+            arr[i][1] = i; //index
         }
 
-        Arrays.sort(arr , (a,b) -> a[0]-b[0]);
+        Arrays.sort(arr, (a,b) -> a[0]-b[0]);
 
         int start = 0;
-        int end = n - 1;
+        int end = n-1;
 
         while(start <= end) {
             int sum = arr[start][0] + arr[end][0];
-
             if(sum == target) {
-                return new int[] {arr[start][1] , arr[end][1]};
+                return new int[]{arr[start][1] , arr[end][1]};
             } else if (sum < target) {
                 start++;
             } else {
                 end--;
             }
         }
+
         return new int[]{-1,-1};
     }
 }
